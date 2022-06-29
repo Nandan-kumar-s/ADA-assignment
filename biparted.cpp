e82e3ad4b49eae2b000fbe720f5ea218b1e31943
Nandan-kumar-s <108354718+Nandan-kumar-s@users.noreply.github.com>
2022-06-29 08:54:43 +0530

    Update biparted.cpp

--- biparted.cpp
+++ biparted.cpp
@@ -1,41 +1,64 @@
-include <iostream>
-#define M 6
-#define N 6
+#include <iostream>
+#define MAX 10
 using namespace std;
-bool bipartiteGraph[M][N] = {
-{0, 1, 0, 0, 0, 0},
-{1, 0, 1, 1, 0, 1},
-{1, 0, 1, 0, 1, 0},
-{0, 1, 1, 0, 0, 1},
-{1, 0, 1, 0, 0, 1},
-{0, 1, 0, 1, 0, 0}
-};
-bool bipartiteMax(int u, bool visited[], int a[]) {
-for (int v = 0; v < N; v++) {
-if (bipartiteGraph[u][v] && !visited[v]) {
-visited[v] = true;
-if (a[v] < 0 || bipartiteMax(a[v], visited, a)) {
-a[v] = u;
-return true;
+int graph[MAX][MAX], setA[MAX], setB[MAX];
+int A,B;
+
+bool bipartiteMatch(int u,bool *visited,int *check)
+{
+    for(int i=0;i<A;i++)
+    {
+        if (graph[u][i] && !visited[i])
+        {
+            visited[i] = true;
+            if (check[i]<0 || bipartiteMatch(check[i],visited,check))
+            {
+                check[i]=u;
+                return true;
+            }
+        }
+    }
+    return false;
 }
+int maxMatch()
+{
+    int check[A];
+    bool visited[A];
+    for(int i=0;i<A;i++)
+        check[i]=-1;
+    int result=0;
+    for(int u=0;u<B;u++)
+    {
+        for(int i=0;i<A;i++)
+            visited[i]=false;
+        if(bipartiteMatch(u,visited,check))
+            result++;
+    }
+    cout<<"Matched vertices set = {";
+    for(int i=0;i<A;i++)
+    {
+        cout<<"("<<setA[i]<<", "<<setB[check[i]]<<")";
+        if(i != A-1)
+            cout<<", ";
+    }
+    cout<<"}";
+    cout<<"\nMaximum number of matching: ";
+    return result;
 }
-}
-return false;
-}
-int maxMatch() {
-int a[N];
-for(int i = 0; i<N; i++)
-a[i] = -1;
-int Count = 0;
-for (int u = 0; u < M; u++) {
-bool visited[N];
-for(int i = 0; i<N; i++)
-visited[i] = false;
-if (bipartiteMax(u, visited, a))
-Count++;
-}
-return Count;
-}
-int main() {
-cout << "Maximum number of applicants matching for a particular job: " << maxMatch();
+int main()
+{
+    cout<<"Enter number of vertices in set A and set B of bipartite graph: ";
+    cin>>A>>B;
+    cout<<"Enter vertices of set A: ";
+    for(int i=0;i<A;i++)
+        cin>>setA[i];
+    cout<<"Enter vertices of set B: ";
+    for(int i=0;i<B;i++)
+        cin>>setB[i];
+    cout<<"Enter "<<A<<" x "<<B<<" adjacency matrix representing relationship between set (A -> B):\n";
+    for(int i=0;i<A;i++)
+        for(int j=0;j<B;j++)
+            cin>>graph[i][j];
+    cout<<maxMatch();
+    return 0;
 }
